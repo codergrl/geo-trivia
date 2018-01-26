5d05e552864d6af4e2b186db34e1547535cff3c2
Chris Warner <cwarner@esri.com>
2018-01-25 16:08:20 -0800

    ACTUALLY add Questions class

---
+++ GeoTrivia/Question.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esri.ArcGISRuntime.Geometry;
+
+namespace GeoTrivia
+{
+    public class Question
+    {
+        private String _text = "";
+        private String _answer = "";
+        private Geometry _geom = null;
+
+        public String Text
+        {
+            get { return _text; }
+        }
+
+        public String Answer
+        {
+            get { return _answer; }
+        }
+
+        public Geometry Geometry
+        {
+            get { return _geom; }
+        }
+
+        public Question(String text, String answer, Geometry geom)
+        {
+            _text = text;
+            _answer = answer;
+            _geom = geom;
+        }
+    }
+}
